eeca189166cad5f23f87ef3359a6d41f46b1205d
Karim <karimbo@live.nl>
2016-09-21 22:44:01 +0200

    Week 4 Exercise

--- Week4/Exercise7.java
+++ Week4/Exercise7.java
@@ -5,5 +5,25 @@ import java.util.Scanner;
  */
 public class Exercise7 {
     public void run(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Amount of tosses?");
+
+        int tosses = sc.nextInt();
+
+        for (int i = 1; i <= tosses; i++){
+
+            int dice = (int) (Math.random() * 6) + 1;
+
+            System.out.println("Ones: " + dice);
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new Exercise7().run();
     }
 }
